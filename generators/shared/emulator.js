00eaf09d0c1e19b90f4700253672ea993748ecf0
cppchriscpp <admin@cpprograms.net>
2023-05-15 22:08:27 -0400

    Work around the obnoxious stream-zip error in the tests

--- generators/shared/emulator.js
+++ generators/shared/emulator.js
@@ -6,7 +6,8 @@ const fs = require('fs'),
     appConfiguration = require('../../config/app-configuration'),
     downloadFile = require('../../util/download-file'),
     findExecutable = require('../../util/find-executable'),
-    spawnAndWait = require('../../util/spawn-and-wait');
+    spawnAndWait = require('../../util/spawn-and-wait'),
+    copyFileSync = require('../../util/copy-file-sync');
 
 async function createConfig(game, directory) {
     
@@ -39,8 +40,9 @@ async function downloadMesen(game, directory) {
     // First download the zip to a known location on disk.
     const mesenInfo = {name: 'Mesen.0.9.9.zip', url: 'https://github.com/SourMesen/Mesen/releases/download/0.9.9/Mesen.0.9.9.zip'};
     const zipFile = path.join(appConfiguration.cacheDirectory, mesenInfo.name);
+    const exeFile = path.join(appConfiguration.cacheDirectory, "Mesen.exe");
     let wasCached = false;
-    if (!fs.existsSync(zipFile)) {
+    if (!fs.existsSync(exeFile)) {
         logger.debug('Downloading mesen from', mesenInfo);
         try {
             await downloadFile(mesenInfo.url, zipFile);
@@ -48,10 +50,26 @@ async function downloadMesen(game, directory) {
             logger.error('Encountered an error downloading mesen binaries; cannot continue.', e);
             throw new Error('Encountered an error downloading mesen binaries; cannot continue.');
         }
+
+        logger.debug('Starting to unzip mesen to temp directory', zipFile);
+        const zip = new StreamZip.async({file: zipFile});
+        try {
+            await zip.extract(null, appConfiguration.cacheDirectory);
+            await zip.close();
+        } catch (e) {
+            logger.error('Failed unzipping mesen to project tools directory', e);
+            logger.info('Extra test info', path.resolve(zipFile), fs.readFileSync(zipFile).toString().substring(0, 100));
+            throw new Error('Failed unzipping mesen to temp directory');
+        }
+        logger.debug('mesen extraction complete.');
+    
     } else {
-        logger.debug('Using cached mesen zip');
+        logger.debug('Using cached mesen exe');
         wasCached = true;
     }
+    logger.debug('Copying exe to tools directory', exeFile);
+    fs.mkdirSync(path.join(directory, 'tools', 'emulators', 'mesen'), {recursive: true});
+    copyFileSync(exeFile, path.join(directory, 'tools', 'emulators', 'mesen', 'Mesen.exe'));
     
     // Now extract it to the tools directory, shifting it around as needed
     try {
@@ -64,17 +82,6 @@ async function downloadMesen(game, directory) {
         }
     }
 
-    logger.debug('Starting to unzip mesen to project tools directory', zipFile);
-    const zip = new StreamZip.async({file: zipFile});
-    try {
-        await zip.extract(null, path.join(directory, 'tools', 'emulators', 'mesen'));
-        await zip.close();
-    } catch (e) {
-        logger.error('Failed unzipping mesen to project tools directory', e, wasCached ? 'Cached version used' : 'Fresh download');
-        logger.info('Extra test info', path.resolve(zipFile), fs.readFileSync(zipFile).toString().substring(0, 100));
-        throw new Error('Failed unzipping mesen to project tools directory');
-    }
-    logger.debug('mesen extraction complete.');
 
     if (process.platform !== 'win32') {
         logger.debug('Non-windows platform, need to chmod the binary');
